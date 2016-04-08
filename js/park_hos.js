4fab738548129b69f4d3ed7da6849fb8a71d6ac6
jireh-he <18772119@qq.com>
2016-04-08 23:19:43 +0800

    #

--- js/park_hos.js
+++ js/park_hos.js
@@ -17,9 +17,7 @@ var rankList=function(data){
 	var xRangeWidth=width-margin.left-margin.right;
 	var yRangeHeight=height-margin.top-margin.bottom;
 	var xScale=d3.scale.linear().domain([0,maxcnt]).range([0,xRangeWidth]);
-	console.log(d3.map(data,function(d) {
-		return d.zdm;		
-	}));
+	console.log(maxcnt);
 	var yScale=d3.scale.ordinal().domain(d3.map(data,function(d) {
 		return d.zdm;		
 	})).rangeRoundBands([0,yRangeHeight],0.1);
